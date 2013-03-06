2ac8f596b84e364e7ad6466961fe948dbafcc4c5
Semen Martynov <semen.martynov@gmail.com>
2013-03-06 06:29:47 +0400

    Bug fix for rotate algorithm

--- CW04-algorithms/rotate.h
+++ CW04-algorithms/rotate.h
@@ -12,47 +12,53 @@
 template<typename It>
 void rotate(It first, It some, It last)
 {
-	last--;
-	while (some != last)
+	It tmp = some;
+	while (first != tmp)
 	{
-		std::iter_swap(first++, some++);
-	}
-	while (first != some)
-	{
-		std::iter_swap(first++, some);
+		// Race
+		std::iter_swap(first++, tmp++);
+		// Reduce the distance.
+		if (tmp == last)
+		{
+			tmp = some;
+		} // New round
+		else if (first == some)
+		{
+			some = tmp;
+		}
 	}
 }
 /*
-//__rotate(first, some, last, typename std::iterator_traits<It>::iterator_category());
+ //__rotate(first, some, last, typename std::iterator_traits<It>::iterator_category());
 
-template<typename BidirectionalIt>
-void __rotate(BidirectionalIt first, BidirectionalIt some, BidirectionalIt last,
-		bidirectional_iterator_tag)
-{
-	BidirectionalIt preLast = last;
-	preLast--;
-	while (some != preLast)
-	{
-		std::iter_swap(first++, some++);
-	}
-	while (first != some)
-	{
-		std::iter_swap(first++, some);
-	}
-}
+ template<typename BidirectionalIt>
+ void __rotate(BidirectionalIt first, BidirectionalIt some, BidirectionalIt last,
+ bidirectional_iterator_tag)
+ {
+ BidirectionalIt preLast = last;
+ preLast--;
+ while (some != preLast)
+ {
+ std::iter_swap(first++, some++);
+ }
+ while (first != some)
+ {
+ std::iter_swap(first++, some);
+ }
+ }
 
-template<typename RandomAccessIt>
-void __rotate<>(RandomAccessIt first, RandomAccessIt some, RandomAccessIt last,
-		random_access_iterator_tag)
-{
-	while (some + 1 != last)
-	{
-		std::iter_swap(first++, some++);
-	}
-	while (first != some)
-	{
-		std::iter_swap(first++, some);
-	}
-}
-*/
+ template<typename RandomAccessIt>
+ void __rotate<>(RandomAccessIt first, RandomAccessIt some, RandomAccessIt last,
+ random_access_iterator_tag)
+ {
+ while (some + 1 != last)
+ {
+ std::iter_swap(first++, some++);
+ }
+ while (first != some)
+ {
+ std::iter_swap(first++, some);
+ }
+ }
+ */
 #endif /* ROTATE_H_ */
